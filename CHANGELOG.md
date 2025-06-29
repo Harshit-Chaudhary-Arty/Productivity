f01bc1bb458de228b65f79e765b1005cbd9388d1
Harshit Chaudhary <harshit.chaudhary.arty@gmail.com>
2025-06-29 23:35:00 +0530

    feat: initial commit with task list UI and local storage
    - Initial structure for task list web app
    - Custom dark mode UI using CSS
    - Functionality to add tasks and save them using localStorage
    - JavaScript logic in `script.js` to handle UI interactions

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+## [0.1.0] - 2025-06-29
+
+### Added
+- Initial structure for task list web app
+- Custom dark mode UI using CSS
+- Functionality to add tasks and save them using localStorage
+- JavaScript logic in `script.js` to handle UI interactions
---
+++ script.js
@@ -0,0 +1,63 @@
+const taskForm = document.querySelector('form');
+const taskInput = document.getElementById('input-task');
+const taskListUL = document.getElementById('task-list');
+
+let allTasks = getTasks();
+updateTaskList();
+
+
+taskForm.addEventListener('submit', function(e){
+    e.preventDefault();
+    addTask();
+})
+
+function addTask(){
+    const taskText = taskInput.value.trim();
+    if(taskText.length > 0){
+        allTasks.push(taskText);
+        updateTaskList();
+        saveTasks();
+        taskInput.value = "";
+    }
+    
+}
+
+function updateTaskList(){
+    taskListUL.innerHTML = "";
+    allTasks.forEach((task, taskIndex)=>{
+        taskItem = createTask(task, taskIndex);
+        taskListUL.append(taskItem);
+    })
+}
+
+
+function createTask(task, taskIndex){
+    const taskId = "task-"+taskIndex;
+    const newTaskLI= document.createElement("li");
+    newTaskLI.className = "task";
+    newTaskLI.innerHTML = `
+                <input type="checkbox" id="${taskId}">
+                <label class="custom-checkbox" for="${taskId}" >
+                    <svg fill="var(--text-muted)" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg>
+                    </svg>
+                </label>
+                <label for="${taskId}" class="task-text"> 
+                    ${task}
+                </label>
+                <button  class="delete-button"><svg fill="var(--text-muted)" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg></button>
+
+    `
+    return newTaskLI;
+}
+
+
+function saveTasks(){
+    const tasksJson = JSON.stringify(allTasks); // converted to strings
+    localStorage.setItem("tasks", tasksJson); //ONLY STRINGS CAN BE STORED AND NOT ARRAYS HERE, SO CONVERTED ARRAY ELEMENTS TO STRING
+
+}
+
+function getTasks(){
+    const tasks = localStorage.getItem("tasks") || "[]"; //if the local storage is empty then it creates empty array instead of giving null
+    return JSON.parse(tasks);
+}
